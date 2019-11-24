aea5e9a80cbac506a8b5d0cc3368fd013e6b09d2
tushmakster <42393011+tushmakster@users.noreply.github.com>
2019-11-24 16:59:16 -0500

    added my code to filter for the date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,36 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function populate(data){ 
+    tbody.text("");
+    data.forEach(function(sighting){
+    var new_tr = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]){
+        new_td = new_tr.append("td").text(value);	
+    });
+})};
+
+populate(tableData);
+
+function onpress(){
+    // Prevents the Page from Refreshing
+    d3.event.preventDefault();
+
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
+
+    // if date is entered
+    if(date) {
+        // create a filter
+        filterData = filterData.filter((row) => row.datetime === date);
+    }
+    // Build Table with Filtered Data
+    populate(filterData);
+}
+
+// update the table
+d3.selectAll("#filter-btn").on("click", onpress);
+
+
